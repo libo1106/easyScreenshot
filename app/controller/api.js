4292e67bc7dfef854b247a10e060f07453dd5fa1
libo <libo1106@foxmail.com>
2018-03-26 23:24:32 +0800

    [*] 修改 API 为 POST 接口 [*] 增加模拟移动端截图的参数

--- app/controller/api.js
+++ app/controller/api.js
@@ -7,19 +7,25 @@ const crypto = require('crypto');
 
 // const phantomjs = require('phantomjs-prebuilt');
 const puppeteer = require('puppeteer');
+const emulateDevices = require('puppeteer/DeviceDescriptors');
 // const bin_path = phantomjs.path;
 
-const worker_pools = [];
+// todo 共享 worker 线程，减少进程启停切换的开销
+// const worker_pools = [];
 
-init();
+// init();
 
 exports.screenshot = function (req, res, next) {
 
-    let worker = worker_puppeteer(req.query.url);
+    let {url, mobile} = req.body;
 
-    worker
-        .then((filename) => {
-            res.redirect(`/output/${filename}`)
+    let worker = worker_puppeteer(url, mobile);
+
+    worker.then((filename) => {
+            res.json({
+                status: 1,
+                data: `/output/${filename}`
+            });
         })
         .catch(next)
 
@@ -33,53 +39,7 @@ exports.prerender = function (req, res, next) {
     res.send('prerender is building!');
 };
 
-/**
- * phantomjs 引擎
- * @deprecated
- * @param target_url
- * @return {Promise}
- */
-function worker_phantomjs (target_url) {
-    const TIMEOUT = 1000 * 10;
-
-    let filename = `${md5(target_url)}.jpeg`;
-    let save_path = path.resolve('./public/output/', filename);
-
-    let args = [
-        path.resolve('./app/phantomjs_script/base_screenshot.js'),
-        target_url,
-        save_path
-    ];
-
-    let worker = child_process.spawn(bin_path, args);
-
-    worker.stdout.pipe(process.stdout);
-    worker.stderr.pipe(process.stderr);
-
-    let timer = setTimeout(() => {
-        worker.kill();
-    }, TIMEOUT);
-
-    return new Promise ((resolve, reject) => {
-
-        worker.on('exit', (code, signal) => {
-
-            clearTimeout(timer);
-
-            if (!signal) {
-                resolve(filename);
-            } else {
-                reject(new Error('截图失败，目标网站没有在10秒内返回 HTML 内容'))
-            }
-
-        });
-
-        worker.on('error', reject)
-
-    })
-}
-
-async function worker_puppeteer (target_url) {
+async function worker_puppeteer (target_url, isMobile = false) {
 
     // 默认超时时间
     const TIMEOUT = 1000 * 10;
@@ -87,10 +47,19 @@ async function worker_puppeteer (target_url) {
     let filename = `${md5(target_url)}.jpeg`;
     let save_path = path.resolve('./public/output/', filename);
 
-    let worker = worker_pools[0];
+    // let worker = worker_pools[0];
+    // await worker.goto(target_url, {timeout: TIMEOUT});
+    // await worker.screenshot({path: save_path});
 
-    await worker.goto(target_url, {timeout: TIMEOUT});
-    await worker.screenshot({path: save_path});
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    if (isMobile) {
+        await page.emulate(emulateDevices['iPhone 6']);
+    }
+    await page.goto(target_url, {timeout: TIMEOUT});
+    await page.screenshot({path: save_path});
+    await browser.close();
 
     return filename;
 }
--- app/router.js
+++ app/router.js
@@ -4,6 +4,7 @@
  * 胶水代码，负责组装请求和实际处理请求的控制器，
  */
 const express  = require('express');
+const bodyParser = require('body-parser');
 const router = express.Router();
 
 let middleware_auth = require('./middleware/auth');
@@ -11,8 +12,12 @@ let middleware_auth = require('./middleware/auth');
 let controller_home = require('./controller/home');
 let controller_api = require('./controller/api');
 
+router.use(bodyParser.json());
+
 router.get('/', controller_home.index);
-router.get('/api/screenshot', middleware_auth.jwt, controller_api.screenshot);
+
+router.post('/api/screenshot', middleware_auth.jwt, controller_api.screenshot);
+
 router.get('/api/prerender', middleware_auth.jwt, controller_api.prerender);
 
 module.exports = router
